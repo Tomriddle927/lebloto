7da7fcacd4847c263c79c919ac1ed9a73e1713c6
Tomriddle927 <mhmdyoussef79@gmail.com>
2025-06-08 13:29:50 +0300

    Update loto_dashboard.py

--- loto_dashboard.py
+++ loto_dashboard.py
@@ -19,7 +19,7 @@ authenticator = stauth.Authenticate(
     preauthorized=config.get('preauthorized', {})
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login", location="main")
 
 if authentication_status:
     st.sidebar.success(f"Welcome {name}")
